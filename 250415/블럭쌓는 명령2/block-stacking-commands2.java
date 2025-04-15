caa5378eb54881f81c517b80342b45b1b82dad7f
shong <52836056+shong69@users.noreply.github.com>
2025-04-15 17:21:51 +0900

    Title: 블럭쌓는 명령2, Time: 137ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250415/블럭쌓는 명령2/block-stacking-commands2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main {
+    static int[] arr = null;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        arr = new int[N];
+        for (int i = 0; i < K; i++) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            chill(arr,A,B);
+        }
+
+        int maxNum = 0;
+        for(int j=0;j<N;j++){
+            maxNum = Math.max(maxNum,arr[j]);
+        }
+       System.out.print(maxNum);
+    }
+    public static void chill(int[] arr, int a, int b){
+        for(int i = a;i<=b;i++){
+            arr[a] += 1;
+        }
+    }
+}
