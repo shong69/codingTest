d5f3c6e6907b64d4547a4a6d15950f6faadf0b5c
shong <52836056+shong69@users.noreply.github.com>
2025-04-24 11:24:33 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 326ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250424/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
+++ 250424/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
@@ -1,47 +1,48 @@
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt(); //n명의 개발자
-        int K = sc.nextInt(); //k번의 악수만 유효함
-        int P = sc.nextInt(); //처음 전염병에 걸린 개발자의 번호
-        int T = sc.nextInt(); //t초에 x개발자와 y개발자가 악수를 나눔
+        int N = sc.nextInt(); // 개발자 수
+        int K = sc.nextInt(); // 감염 가능 횟수
+        int P = sc.nextInt(); // 처음 감염자
+        int T = sc.nextInt(); // 악수 수
 
-        int[] infectCount = new int[N];//감염 가능 횟수
-        boolean[] infected = new boolean[N];
-        infected[P-1] = true; 
-        infectCount[P-1] = K;
+        int[] infectCount = new int[N];
+        int[] infected = new int[N];
+        infected[P - 1] = 1;
+        infectCount[P - 1] = K;
+
         int[][] shakes = new int[T][3];
-        for (int i = 0; i < T; i++) { //T초 동안 누구와 누가 악수 했는지
-            shakes[i][0] = sc.nextInt();
-            shakes[i][1] = sc.nextInt();
-            shakes[i][2] = sc.nextInt();
+        for (int i = 0; i < T; i++) {
+            shakes[i][0] = sc.nextInt(); // 시간
+            shakes[i][1] = sc.nextInt(); // x
+            shakes[i][2] = sc.nextInt(); // y
         }
-        //첫번째 인자 값 기준으로 정렬
-        Arrays.sort(shakes,(a,b) -> Integer.compare(a[0],b[0]));
-        
-        for(int i=0;i<shakes.length;i++){
-            int time = shakes[i][0];
-            int x = shakes[i][1]-1;
-            int y = shakes[i][2]-1;
-            if (infected[x] && infectCount[x] > 0) {
-                if (!infected[y]) {
-                    infected[y] = true;
-                    infectCount[y] = K; // y도 감염자가 됨
-                }
+
+        Arrays.sort(shakes, (a, b) -> Integer.compare(a[0], b[0]));
+
+        for (int i = 0; i < T; i++) {
+            int x = shakes[i][1] - 1;
+            int y = shakes[i][2] - 1;
+
+            // x가 감염자고 y는 아직이면
+            if (infected[x] == 1 && infectCount[x] > 0 && infected[y] == 0) {
+                infected[y] = 1;
+                infectCount[y] = K;
                 infectCount[x]--;
             }
 
-            if(infected[y]&&infectCount[y] > 0) {
-                if(!infected[x]) {
-                    infected[x] = true;
-                    infectCount[x] = K;
-                }
+            // y가 감염자고 x는 아직이면
+            if (infected[y] == 1 && infectCount[y] > 0 && infected[x] == 0) {
+                infected[x] = 1;
+                infectCount[x] = K;
                 infectCount[y]--;
             }
         }
-        for(int i=0;i<N;i++){
-            System.out.print(infected[i]?1:0);
+
+        for (int i = 0; i < N; i++) {
+            System.out.print(infected[i]);
         }
     }
 }
