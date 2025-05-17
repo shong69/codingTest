98f703400625d04e3e2a479e38d551edfcc2bcb4
shong <52836056+shong69@users.noreply.github.com>
2025-05-18 01:51:53 +0900

    Title: 체크판위에서 2, Time: 135ms, Memory: 10MB, Status: Passed - Codetree

--- 250518/체크판위에서 2/on-the-checkboard-2.java
+++ 250518/체크판위에서 2/on-the-checkboard-2.java
@@ -14,8 +14,8 @@ public class Main {
         //점프해서 도달한 위치가 2곳이어야 함
         //첫번째, 두번째로 방문하는 위치를 일일히 잡아서 다른지 확인해준다. 
         int result = 0;
-        for(int i=0;i<R;i++){
-            for(int j=0;j<C;j++){
+        for(int i=1;i<R;i++){
+            for(int j=1;j<C;j++){
                 for(int k=i+1;k<R-1;k++){ 
                     for(int l=j+1;l<C-1;l++){
                         if(grid[0][0] != grid[i][j] && //첫번째랑 두번쨰 색이 달라야 한다
